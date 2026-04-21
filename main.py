94269d2d3d28087b3e15a3a92089db31d3f2316c
Alina <kazieva.am@phystech.edu>
2026-04-22 00:16:42 +0300

    Код приведен в порядок

--- main.py
+++ main.py
@@ -1,73 +1,24 @@
-import sys
 import pygame as pg
-from pygame.locals import *
 
-screen = pg.display.set_mode((1700, 1000))
-GLOBAL_SCALE=0.6
-GLOBAL_X=0
-GLOBAL_Y=0
-PRESSED_NOW=False
-
-class Tile:
-    _x_pos = 0
-    _y_pos = 0
-    _size = 64
-
-    def __init__(self, sprite_path="./tiles.png", x = 0, y = 0, size = 256):
-        self._sprite = pg.image.load(sprite_path).subsurface((512, 512, size, size)).copy()
-        self._x_pos = x
-        self._y_pos = y
-        self._size = size
-
-    def draw(self):
-        scaled_size = int(self._size * GLOBAL_SCALE)
-        scaled_sprite = pg.transform.scale(self._sprite, (scaled_size, scaled_size))
-        screen.blit(scaled_sprite, (self._x_pos * scaled_size + GLOBAL_X + delta_x,
-                                          self._y_pos * scaled_size + GLOBAL_Y + delta_y))
+from src.Graphics import Graphics
+from src.Tile import Tile
 
 
-tile1 = Tile(x=1, y=0)
-tile2 = Tile(x=0, y=2)
-
-old_mouse_x, old_mouse_y = -1, -1
-new_mouse_x, new_mouse_y = 0, 0
-delta_x, delta_y = 0, 0
+tile1 = Tile(5, 2, 0, 0)
+tile2 = Tile(2, 1, 0, -1)
+tile3 = Tile(10, 1, 1, 0)
+window = Graphics()
 
 while True:
     # 1 - обработка событий
     for event in pg.event.get():
-        screen.set_colorkey()
-        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-            pg.quit()
-            sys.exit()
-        elif event.type == MOUSEWHEEL:
-            if event.y > 0:
-                GLOBAL_SCALE += 0.2
-            else:
-                GLOBAL_SCALE -= 0.2
-                GLOBAL_SCALE = max(0.2, GLOBAL_SCALE)
-
-        buttons = pg.mouse.get_pressed()
-        if buttons[0]:
-            if (not PRESSED_NOW):
-                old_mouse_x, old_mouse_y = pg.mouse.get_pos()
-                PRESSED_NOW=True
-            else:
-                new_mouse_x, new_mouse_y = pg.mouse.get_pos()
-                delta_x = new_mouse_x - old_mouse_x
-                delta_y = new_mouse_y - old_mouse_y
-        if not buttons[0] and PRESSED_NOW:
-            GLOBAL_X += delta_x
-            GLOBAL_Y += delta_y
-            delta_x, delta_y = 0, 0
-            PRESSED_NOW=False
-
-
+        window.handle_event(event)
 
     # 2 - обновление логики и параметров
 
     # 3 - Отрисовка экрана и объектов
-    screen.fill(pg.Color(80, 50, 20))
-    tile1.draw()
-    tile2.draw()
+    window.draw()
+    tile1.draw(window)
+    tile2.draw(window)
+    tile3.draw(window)
     pg.display.update()
